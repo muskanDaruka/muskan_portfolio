de5ac0999f79115e98afd95b1d6837b4ce1e2b9e
muskanDaruka <darukamuskan@gmail.com>
2026-07-30 14:48:06 +0530

    refactor: simplify next.config.ts by removing basePath and assetPrefix logic

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
 import type { NextConfig } from "next";
 
-  const isProd = process.env.NODE_ENV === "production";
+const nextConfig: NextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+};
 
-  const nextConfig: NextConfig = {
-    output: "export",
-    images: {
-      unoptimized: true,
-    },
-    basePath: isProd ? "/muskan_portfolio" : "",
-    assetPrefix: isProd ? "/muskan_portfolio/" : "",
-  };
-
-  export default nextConfig;
+export default nextConfig;
